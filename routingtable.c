efaba2d90b1e45986b16b20c763338b6d3196f39
Ben Jiang <jiang551@purdue.edu>
2020-11-02 01:42:25 -0500

    Oops

--- routingtable.c
+++ routingtable.c
@@ -39,10 +39,10 @@ int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myI
 	//if next_hop(x,y)=z|(d<d(x,y) &x=nexthop(z,y)) force updated
 	//else ....
 	int i;
-	for (i = 0; i < InitResponse->no_nbr; i++){
+	for (i = 0; i < RecvdUpdatePacket->no_routes; i++){
 		//total cost INF check
 
-		
+
 	}
 	return updated;
 }
