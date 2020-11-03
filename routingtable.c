5f083bbfa3458af5e0cde00166d9d52a74ff1c2d
trechoy <77rgh77@gmail.com>
2020-11-02 23:18:18 -0500

    fixed memcpy?

--- routingtable.c
+++ routingtable.c
@@ -63,7 +63,7 @@ int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myI
 			}
 			routingTable[destID].path_len = currRoute->path_len + 1;
 			routingTable[destID].path[0]=myID;
-			memcpy(routingTable[destID].path[1],currRoute->path,(currRoute->path_len)*sizeof(int));
+			memcpy(&(routingTable[destID].path[1]),currRoute->path,(currRoute->path_len)*sizeof(int));
 		}
 		//forced update
 		else if (routingTable[destID].next_hop == RecvdUpdatePacket->sender_id){
@@ -78,7 +78,7 @@ int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myI
 			}
 			routingTable[destID].path_len = currRoute->path_len + 1;
 			routingTable[destID].path[0]=myID;
-			memcpy(routingTable[destID].path[1],currRoute->path,(currRoute->path_len)*sizeof(int));
+			memcpy(&(routingTable[destID].path[1]),currRoute->path,(currRoute->path_len)*sizeof(int));
 		}
 		else{//split horizon
 			if((currRoute->cost + costToNbr) < routingTable[destID].cost){
@@ -100,7 +100,7 @@ int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myI
 					}
 					routingTable[destID].path_len = currRoute->path_len + 1;
 					routingTable[destID].path[0]=myID;
-					memcpy(routingTable[destID].path[1],currRoute->path,(currRoute->path_len)*sizeof(int));
+					memcpy(&(routingTable[destID].path[1]),currRoute->path,(currRoute->path_len)*sizeof(int));
 				}
 			}
 		}
