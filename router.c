c3fb048c37bfa030eac549588680e562347e2c53
trechoy <77rgh77@gmail.com>
2020-11-03 00:20:23 -0500

    added code from last project

--- router.c
+++ router.c
@@ -7,6 +7,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+//from the notes :)
+int open_udpfd(int port)
+{
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        return -1;
+
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+                   (const void *)&optval , sizeof(int)) < 0)
+        return -1;
+
+    /* Listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)port);
+    if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+
+    return listenfd;
+}
+
 
 int main (int argc, char ** argv)
 {
